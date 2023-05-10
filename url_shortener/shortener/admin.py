aab563fb8614dc9ed14993da9101fa6ccea53620
Ali <alisantos@onsigntv.com>
2023-05-10 18:03:11 -0300

    List urls on Admin page

--- url_shortener/shortener/admin.py
+++ url_shortener/shortener/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import URL
 
 # Register your models here.
+
+
+class URLAdmin(admin.ModelAdmin):
+    list_display = ("original_url", "short_url", "created_at", "access_count")
+    readonly_fields = ("short_url", "access_count", "created_at")
+
+
+admin.site.register(URL, URLAdmin)
