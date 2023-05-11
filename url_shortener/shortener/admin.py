4429388a42ef1523dd58a4551be42e51990394d1
Ali <alisantos@onsigntv.com>
2023-05-11 09:55:13 -0300

    Add delete option to admin

--- url_shortener/shortener/admin.py
+++ url_shortener/shortener/admin.py
@@ -6,7 +6,13 @@ from .models import URL
 
 class URLAdmin(admin.ModelAdmin):
     list_display = ("original_url", "short_url", "created_at", "access_count")
+    search_fields = ("original_url", "short_url")
     readonly_fields = ("short_url", "access_count", "created_at")
+
+    def delete_selected(self, request, queryset):
+        queryset.delete()
+
+    delete_selected.short_description = "Delete selected URLs"
 
 
 admin.site.register(URL, URLAdmin)
